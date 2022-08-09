3549e5597c02de8ec56c04e0e04bb59adbe15ef5
Hanz <22578473+fivehanz@users.noreply.github.com>
2022-08-10 02:58:04 +0530

    chore: rc

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,29 @@
 import React from "react";
+import polygonLogo from "../assets/polygonlogo.png";
+import ethLogo from "../assets/ethlogo.png";
 
-const Header = () => {
+const Header = ({ currentAccount, network }) => {
   return (
     <div className="header-container">
       <header>
         <div className="left">
           <p className="title">🐱‍👤 Ultra Cool Name Service</p>
           {/* <p className="subtitle">Your immortal API on the blockchain!</p> */}
+        </div>
+        <div className="right">
+          <img
+            alt="Network logo"
+            className="logo"
+            src={network.includes("Polygon") ? polygonLogo : ethLogo}
+          />
+          {currentAccount ? (
+            <p>
+              {" "}
+              Wallet: {currentAccount.slice(0, 6)}...{currentAccount.slice(-4)}{" "}
+            </p>
+          ) : (
+            <p> Not connected </p>
+          )}
         </div>
       </header>
     </div>
